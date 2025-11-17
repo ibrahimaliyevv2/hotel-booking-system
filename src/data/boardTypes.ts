c797554ef137b5a432a0dc7a95f6b11af6409d6f
ibrahimaliyevv2 <7caucb@code.edu.az>
2025-11-17 22:32:47 +0400

    added data

---
+++ src/data/boardTypes.ts
@@ -0,0 +1,5 @@
+export const boardTypes = [
+  { code: "FB", name: "Full Board" }, // Breakfast, Lunch & Dinner included
+  { code: "HB", name: "Half Board" }, // Breakfast & one meal included
+  { code: "NB", name: "No Board" }, // No meals included
+];
