ab4cae45b2ffba722f25e3f2d9bf23774100421c
ibrahimaliyevv2 <7caucb@code.edu.az>
2025-11-19 01:54:55 +0400

    feat(BookingForm): add selecting citizenship

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import BookingForm from "./components/BookingForm";
+
 function App() {
   return (
     <>
       <p className="text-red-500">Hello</p>
+      <BookingForm />
     </>
   );
 }
--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,5 +1,32 @@
+import { useAppDispatch, useAppSelector } from "../redux/store";
+import { countries } from "../data/countries";
+import { setCitizenship } from "../redux/slices/bookingSlice";
 const BookingForm = () => {
-  return <div></div>;
+  const dispatch = useAppDispatch();
+  const config = useAppSelector((state) => state.booking.booking);
+  return (
+    <form>
+      <h1>Booking configuration</h1>
+
+      <div>
+        <div>
+          <label>Citizenship:</label>
+          <select
+            value={config.citizenship}
+            onChange={(e) => dispatch(setCitizenship(e.target.value))}
+            className="border p-2 w-full"
+          >
+            <option value="">Select country</option>
+            {countries.map((country) => (
+              <option key={country.id} value={country.name}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default BookingForm;
